27c5ec1ca6cc2979d6310dd80bd58f0f8e45a34c
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-05-17 22:42:29 +0530

    Time: 21 ms (15.47%) | Memory: 58.1 MB (59.56%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -10,3 +10,11 @@ class Solution {
         return false;
     }
 }
+
+
+// Step 3: Iterate from i = 1 to i = 4
+// i	nums[i]	nums[i-1]	Condition nums[i] == nums[i-1]?	Action
+// 1	2	1	2 == 1? No	Continue loop
+// 2	3	2	3 == 2? No	Continue loop
+// 3	4	3	4 == 3? No	Continue loop
+// 4	4	4	4 == 4? Yes	Return true (duplicate found)
