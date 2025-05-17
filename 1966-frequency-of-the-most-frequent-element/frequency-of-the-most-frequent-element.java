f3baafa88dc5c379d1a5c3664c360014a7b71b34
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-05-17 18:32:10 +0530

    Time: 29 ms (88.79%) | Memory: 56.4 MB (90.85%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,23 +1,17 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        int maxFrequency = 0; // Initialize the maximum frequency
-        long currentSum = 0; // Initialize the current sum of the subarray elements
+        Arrays.sort(nums);
+        long sum = 0;
+        int start = 0;
+        int count = 0;
 
-        Arrays.sort(nums); // Sort the array in ascending order
-
-        for (int left = 0, right = 0; right < nums.length; ++right) {
-            currentSum += nums[right]; // Include the current element in the subarray sum
-
-            // Check if the current subarray violates the condition (sum + k < nums[right] * length)
-            while (currentSum + k < (long) nums[right] * (right - left + 1)) {
-                currentSum -= nums[left]; // Adjust the subarray sum by removing the leftmost element
-                left++; // Move the left pointer to the right
+        for(int end = 0;end<nums.length;end++){
+            sum += nums[end];
+            while( ( ( (long)nums[end]*(end-start+1) )-sum) > k && start <= end){
+                sum -= nums[start++];
             }
-
-            // Update the maximum frequency based on the current subarray length
-            maxFrequency = Math.max(maxFrequency, right - left + 1);
+            count = Math.max(count,end-start+1);
         }
-
-        return maxFrequency;
+        return count;
     }
 }
