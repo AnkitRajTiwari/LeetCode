7127bc71cf733f24c041381a841dbe22e1a7ecd7
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-05-17 18:13:06 +0530

    Time: 57 ms (51.63%) | Memory: 60.9 MB (30.79%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
@@ -1,23 +1,34 @@
 class Solution {
     public long countGood(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        long count = 0, result = 0;
-        int left = 0;
+         int n = nums.length;   // Total number of elements
+        int i = 0, j = 0;      // Two pointers for the sliding window
 
-        for (int right = 0; right < nums.length; right++) {
-            int val = nums[right];
-            int currentFreq = freq.getOrDefault(val, 0);
-            count += currentFreq;
-            freq.put(val, currentFreq + 1);
+        long result = 0;       // To store the final answer
+        long pairs = 0;        // To store how many good pairs we have in the current window
 
-            while (count >= k) {
-                result += nums.length - right; 
-                int leftVal = nums[left];
-                int leftFreq = freq.get(leftVal);
-                freq.put(leftVal, leftFreq - 1);
-                count -= leftFreq - 1;
-                left++;
+        HashMap<Integer, Integer> map = new HashMap<>(); // Frequency map for counting elements
+
+        while (j < n)
+        {
+            // Add number of times nums[j] has already appeared to the pairs
+            // Because each previous occurrence of nums[j] will make a new pair with nums[j]
+            pairs += map.getOrDefault(nums[j], 0);
+            
+            // Increase the frequency of nums[j]
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            // If we have enough pairs (i.e., pairs >= k), we try to move left pointer i to reduce the window size
+            while (pairs >= k)
+            {
+                result += (n - j);  // All subarrays from i to end with this j are valid
+
+                // Before moving i, reduce frequency and pair count
+                map.put(nums[i], map.get(nums[i]) - 1);
+                pairs -= map.get(nums[i]);  // Remove the pairs formed by nums[i] in this window
+                i++;  // Shrink the window from left
             }
+
+            j++; // Expand the window from right
         }
 
         return result;
