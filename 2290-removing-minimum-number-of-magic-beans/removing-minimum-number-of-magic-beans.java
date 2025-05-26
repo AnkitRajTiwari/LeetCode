547b1c719de153e0098936e6259984fad1c10487
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-05-26 22:14:10 +0530

    Time: 35 ms (61.07%) | Memory: 64 MB (56.97%) - LeetSync

---
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.java
@@ -0,0 +1,11 @@
+class Solution {
+        public long minimumRemoval(int[] beans) {
+        long mx = 0, sum = 0;
+        Arrays.sort(beans);
+        for (int i = 0, n = beans.length; i < n; ++i) {
+            sum += beans[i];
+            mx = Math.max(mx, (long)beans[i] * (n - i));
+        }
+        return sum - mx;
+    }
+ }
