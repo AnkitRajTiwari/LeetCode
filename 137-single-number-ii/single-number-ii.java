eaef62ba2ef920328f3d05f3aa4afde03870a58d
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-06-03 22:59:26 +0530

    Time: 4 ms (55.86%) | Memory: 46.1 MB (6.65%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,22 +1,35 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0, twos = 0;
+        int result = 0;
 
-        for (int num : nums) {
-            // Add bits to 'twos' that are already in 'ones' and now appear again in num
-            twos |= ones & num;
+        // Check all 32 bit positions
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
 
-            // XOR current number with 'ones'
-            ones ^= num;
+            // Count how many numbers have the i-th bit set
+            for (int num : nums) {
+                if ((num >> i & 1) == 1) {
+                    sum++;
+                }
+            }
 
-            // Calculate the common bits in both 'ones' and 'twos' (i.e., bits that appeared 3 times)
-            int threes = ones & twos;
-
-            // Remove common bits (threes) from both ones and twos
-            ones &= ~threes;
-            twos &= ~threes;
+            // If bit count is not divisible by 3, that bit is set in the unique number
+            if (sum % 3 != 0) {
+                result |= (1 << i);
+            }
         }
 
-        return ones;
+        return result;
     }
 }
+
+ // another approach 
+  //  class Solution {
+  //  public int singleNumber(int[] arr) {
+  //  int ones = 0, twos = 0;
+  //  for (int i : arr) {
+  //      ones = (ones ^ i) & ~twos; // ones ^ i, XOR i with current ones., If the bit is  /
+                                                      // already in ones, this will remove it 
+  //      twos = (twos ^ i) & ~ones; // 
+  //  }
+  //  return ones;
