cc1de5b8f8689dd45f85d0943747a2e48e37a121
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-06-07 09:39:36 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (29.60%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,16 +3,11 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
-         // since given that the consecutive same element will be at the even and odd so 
-        // if mid is divided by  2 NS GET 1 MENS IT IS AT ODD 
-
-            if (mid % 2 == 1) {
-                mid--;
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid] ==nums[mid-1])){
+                left=mid+1;
             }
-            if (nums[mid] != nums[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 2;
+            else{
+                right=mid;
             }
         }
         return nums[left];
