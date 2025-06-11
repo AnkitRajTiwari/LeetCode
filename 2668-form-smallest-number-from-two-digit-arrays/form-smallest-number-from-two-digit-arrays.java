f73656f2a0173f52090d2283df3d0cedcce209f9
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-06-11 12:51:37 +0530

    Time: 1 ms (82.79%) | Memory: 41.9 MB (38.11%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
@@ -1,11 +1,16 @@
 class Solution {
     public int minNumber(int[] nums1, int[] nums2) {
+        // sort both in the ascending order 
         Arrays.sort(nums1);
         Arrays.sort(nums2);
 
         int n1 = nums1.length;
         int n2 = nums2.length;
         int result= 0;
+
+        //If a digit from nums1 is equal to a digit from nums2, that means we found a common 
+        // digit.
+        // Save it in result and return it immediately, because due to sorting, this will be  //  the smallest possible common digit.
         for(int i = 0 ; i< n1; i++){
             for(int j = 0 ; j <n2; j++ ){
                 if(nums1[i] == nums2[j]){
