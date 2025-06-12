191411683e6608ad3613bdc3bfb521c7666ee895
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-06-12 21:22:35 +0530

    Time: 5 ms (36.35%) | Memory: 45.3 MB (79.74%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,25 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-
-        // Check all 32 bit positions
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
-
-            // Count how many numbers have the i-th bit set
-            for (int num : nums) {
-                if ((num >> i & 1) == 1) {
-                    sum++;
-                }
-            }
-
-            // If bit count is not divisible by 3, that bit is set in the unique number
-            if (sum % 3 != 0) {
-                result |= (1 << i);
+        Arrays.sort(nums);
+        if(nums.length==1)return nums[0];
+        if(nums[0]!=nums[1])return nums[0];
+        for(int i=1;i<nums.length-2;i++)
+        {
+            if(nums[i]!=nums[i-1]&& nums[i]!=nums[i+1])
+            {
+                return nums[i];
             }
         }
-
-        return result;
+        return nums[nums.length-1];
     }
 }
 
