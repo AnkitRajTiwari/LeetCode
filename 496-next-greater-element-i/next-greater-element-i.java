2d171e78d1ec42aa3c3bd5e6433f632ad30c0293
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-06-13 09:35:21 +0530

    Time: 2 ms (99.11%) | Memory: 43.5 MB (78.39%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[]=new int[nums1.length];
-
-        for(int i=0;i<nums1.length;i++)
-        {
-            int greater=-1;
-            int j=nums2.length-1;
-            while(j>=0 && nums2[j] !=nums1[i])
-            {
-                if(nums2[j]>nums1[i])
-                {
-                    greater=nums2[j];
-                }
-                j--;
-            }
-            ans[i]=greater;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            mp.put(nums2[i], i);
         }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = -1;
+            int curr = mp.get(nums1[i]) + 1;
+            while (curr < nums2.length) {
+                if (nums2[curr] > nums1[i]) {
+                    ans[i] = nums2[curr];
+                    break;
+                }
+                curr++;
+            }
+        }
+
         return ans;
     }
 }
