19006f40cafdc0be17e739feed4ffad46adab2af
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-06-16 09:44:14 +0530

    Time: 55 ms (100.00%) | Memory: 57.5 MB (47.53%) - LeetSync

--- 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.java
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.java
@@ -1,22 +1,31 @@
 class Solution {
-  public long maximumSubarraySum(int[] nums, int k) {
-    long ans = Long.MIN_VALUE;
-    long prefix = 0;
-    Map<Integer, Long> hm = new HashMap<>();
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length ; 
+        long rs = Long.MIN_VALUE ; 
+        long sum = 0L ; 
+        Map< Long , Long > map = new HashMap<>(n) ;  
+        map.put( (long) nums[0] , 0L ) ; 
+        sum = nums[0] ; 
+        for( int i=1 ; i<n ; ++i ){
+            long first = nums[i] - k ; 
+            Long f = map.get( first ) ; 
+            if( f != null ){
+                rs = Math.max( rs , sum + nums[i] - f ) ; 
+            }
+            long second = nums[i] + k ; 
+            Long s = map.get( second ) ; 
+            if( s != null ){
+                rs = Math.max( rs , sum + nums[i] - s ) ; 
+            }
 
-    for (final int num : nums) {
-    if(!hm.containsKey(num)||hm.get(num)>prefix){
-        hm.put(num,prefix);
+            Long curr = map.get( (long) nums[i] ) ; 
+            if( curr  == null ){
+                map.put( (long) nums[i] , sum ) ; 
+            }else if( sum < curr ){
+                map.put( (long) nums[i] , sum ) ; 
+            }
+            sum += nums[i] ;
+        }
+        return rs == Long.MIN_VALUE ? 0 : rs ; 
     }
-    prefix+=num;
-    if(hm.containsKey(num-k)){
-        ans=Math.max(ans,prefix-hm.get(num-k));
-    }
-    if(hm.containsKey(num+k)){
-        ans=Math.max(ans,prefix-hm.get(num+k));
-    }
-    }
-
-    return ans == Long.MIN_VALUE ? 0 : ans;
-  }
 }
