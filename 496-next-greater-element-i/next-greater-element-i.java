34afcc58a22b2070ca1ad8d4a77e84c7bf0794b0
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-07-09 00:31:22 +0530

    Time: 2 ms (99.19%) | Memory: 43.5 MB (73.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // a hashmap to store the index of num2 so that 
+        // we can check where did the num1 in the 2 apopears 
         HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < nums2.length; i++) {
-            mp.put(nums2[i], i);
-        }
+            mp.put(nums2[i], i); // Example: if nums2 = [4,1,2], then map = {4:0, 1:1, 2:2}
+        } 
+        // an array to store the finasl 
         int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            ans[i] = -1;
-            int curr = mp.get(nums1[i]) + 1;
+            ans[i] = -1; 
+            // store the index of nums1 in num 2 and then check for another 
+            int curr = mp.get(nums1[i]) + 1; 
+           // Search for the next greater element to the right in nums2
             while (curr < nums2.length) {
+
                 if (nums2[curr] > nums1[i]) {
                     ans[i] = nums2[curr];
                     break;
@@ -16,7 +22,6 @@ class Solution {
                 curr++;
             }
         }
-
         return ans;
     }
 }
