79a7e5ec2ae9e69ae38b6e38b0d47829621c65de
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2025-07-11 00:24:20 +0530

    Time: 34 ms (67.35%) | Memory: 55 MB (52.86%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {             
+        // we will sort them firt in the duratiuon wise (
+        Arrays.sort(courses,(a,b)-> a[1]==b[1] ? a[0]-b[0] : a[1]-b[1]);
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b) -> b-a);
+         int time=0;
+         for(int[] course:courses){
+            // check if the course can beco,pleted within the time frame 
+            if(course[0]<=course[1]){
+                if(course[0]+time<=course[1]){
+                    pq.offer(course[0]);
+                    time+=course[0];
+                }
+                else{
+                    if(pq.peek() > course[0]){
+                        time-=pq.poll();
+                        time+=course[0];
+                        pq.offer(course[0]);
+                    }
+                }
+            }
+         }
+         return pq.size();
+        
+    }
+}
