87654116667cd5a9e12ac94c5fe07ae55dc02093
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2026-08-11 21:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (59.21%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,25 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        ListNode slow = head;
+        /// handle single node linked list 
+        if(head.next==null) return null;
 
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
-        }
+         //// assign fast and slow pointers 
+         ListNode slow=head;
+         ListNode fast=head;
 
-        if (fast == null) {
-            return head.next;  // remove the head node
-        }
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
+         // move the fast n times to create a gap 
+         int i=1;
+         while(i<=n)
+         {
+            fast=fast.next;
+            i++;
+         }
 
-        // Skip the nth node from end
-        slow.next = slow.next.next;
+         // if fast becomes nunll means we have to remove the head 
+         if(fast == null) return head.next;
+         
+         // move both until the fast point to the end of the node 
+         while(fast.next!=null)
+         {
+            slow=slow.next;
+            fast=fast.next;
+         }
 
-        return head;
+         //// remove the nth node 
+
+         if(slow.next!=null)
+         {
+            slow.next=slow.next.next;
+         }
+         return head;
     }
 }
-
-
