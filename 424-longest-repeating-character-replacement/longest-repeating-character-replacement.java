ddaa4963ead87ec9602f7a96a2e71545065de4e0
Ankit Raj Tiwari <ankittiwari.at2026@gmail.com>
2026-08-18 21:30:31 +0530

    Time: 35 ms (14.36%) | Memory: 46.8 MB (26.17%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,24 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int ans = 0;
-        int n = s.length();
-        for (char c = 'A'; c <= 'Z'; c++) {
-            int i = 0, j = 0, replaced = 0;
-            while (j < n) {
-                if (s.charAt(j) == c) {
-                    j++;
-                } else if (replaced < k) {
-                    j++;
-                    replaced++;
-                } else if (s.charAt(i) == c) {
-                    i++;
-                } else {
-                    i++;
-                    replaced--;
-                }
-                ans = Math.max(ans, j - i);
-            }
-        }
-        return ans;
+        ///// here sliding window + two pointer + frequency count 
+        //// we have to find the maxfreq inside the current window
+        HashMap<Character, Integer> map=new HashMap<>();
+        int start=0;
+        int answer=0;
+        int largestcount=0;
+        for(int end=0; end<s.length(); end++)
+        {
+            /// count the character 
+              char ch=s.charAt(end);
+              map.put(ch, map.getOrDefault(ch,0)+1);
+              largestcount=Math.max(largestcount, map.get(ch));
+
+              /// equation window size-largestcount
+              if(end-start+1 - largestcount>k)
+              {
+                char chh= s.charAt(start);
+                map.put(chh, map.get(chh) - 1);
+                start++;
+              }
+              answer=Math.max(answer,end-start+1);
+        } 
+        return answer ;
     }
 }
